630981405a80a8dbb98a68e46fae5391bb5cebf2
Suzy Roy <suzy@iamsuzyq.com>
2023-03-06 20:50:41 -0800

    Added more quotes

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -6,8 +6,12 @@ export default function Quotes() {
 '“Any fool can write code that a computer can understand. Good programmers write code that humans can understand.” - Martin Fowler',
 '“Common programmer thought pattern: there are only three numbers: 0, 1, and n.” - Joel Spolsky',
 '“One of my most productive days was throwing away 1,000 lines of code.” - Ken Thompson',
-'“Scrum is like your mother-in-law, it points out ALL your faults.” - Ken Schwaber']
+'“Scrum is like your mother-in-law, it points out ALL your faults.” - Ken Schwaber',
+'“If debugging is the process of removing software bugs, then programming must be the process of putting them in” - Edsger Dijkstra',
+'“The greatest risk we face in software development is that of overestimating our own knowledge.” - Jim Highsmith',
+'“I have not failed. I\'ve just found 10,000 ways that won\'t work.” - Thomas Edison',
+'“Code as if the next guy to maintain your code is a homicidal maniac who knows where you live.” - Kathy Sierra']
 return (
-    quoteOptions[2]
+    quoteOptions[6]
 )
 }

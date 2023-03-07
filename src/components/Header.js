60de55c8efa553186daeda986b3244ea520b1dc5
Suzy Roy <suzy@iamsuzyq.com>
2023-03-06 20:43:33 -0800

    Added Header and Quotes component starts

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import '../App.css'
+import Quotes from './Quotes'
+
+export default function Header() {
+    return (
+        <>
+        <Quotes />
+        <header><h1>Suzy M Roy</h1></header>
+        </>
+        
+        )
+    }
---
+++ src/components/Quotes.js
@@ -0,0 +1,13 @@
+import '../App.css'
+
+export default function Quotes() {
+    const quoteOptions = ['“Programming isn\'t about what you know; it\'s about what you can figure out.” - Chris Pine',
+'“Software and cathedrals are much the same; first we build them, then we pray.” - Anonymous',
+'“Any fool can write code that a computer can understand. Good programmers write code that humans can understand.” - Martin Fowler',
+'“Common programmer thought pattern: there are only three numbers: 0, 1, and n.” - Joel Spolsky',
+'“One of my most productive days was throwing away 1,000 lines of code.” - Ken Thompson',
+'“Scrum is like your mother-in-law, it points out ALL your faults.” - Ken Schwaber']
+return (
+    quoteOptions[2]
+)
+}
